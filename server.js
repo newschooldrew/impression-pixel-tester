8ebb6164ca4e9b376949512bf698cd79330144b4
Andrew Perez <andrewperez@Andrews-MacBook-Air.local>
2020-08-02 16:04:20 -0700

    adding in yargs to build filename from command line

--- server.js
+++ server.js
@@ -4,47 +4,59 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const axios = require('axios')
 const neatCsv = require('neat-csv');
+const args = require('yargs').argv;
 
 let arr = [];
-let errArr = [];
-fs.readFile(__dirname + "/" + process.argv.slice(2), async (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
+let mappedUrls = [];
+let finalURL;
+
+
+console.log('Error: ' + args.error);
+const errArg = args.error;
+console.log('data: ' + args.data);
+const dataArg = args.error;args.data;
+console.log('csv: ' + args.csv);
+const csvArg = args.csv;
+
+
+// Read incoming CSV file as argument the user has input
+fs.readFile(__dirname + "/" + csvArg, async (err, data) => {
+
   const res = await neatCsv(data)
-
   res.map(r =>{
-    console.log(r)
+    // console.log(r)
     const newRow = r.impression_pixel_json;
-
     const strippedQuotes = newRow.replace(/['"]+/g, '')
     const strippedBrackets = strippedQuotes.replace(/[\[\]']/g,'' );
-    const finalURL = strippedBrackets.replace(/\\/g, '')
-    arr.push(finalURL)
-
-    const tac = r.tactic_id;
-    errArr.push({tac,impression_pixel_json:finalURL})
-
+    const noBackslashes = strippedBrackets.replace(/\\/g, '');
+    if (noBackslashes.indexOf(',') > -1) { 
+      finalURL = noBackslashes.substr(0, noBackslashes.indexOf(','))
+      // arr.push(firstURLinBunch)
+    }else{
+      finalURL = noBackslashes;
+    }
+    const tactic_id = r.tactic_id;
+    mappedUrls.push({tactic_id,impression_pixel_json:finalURL})
+    // console.log(mappedUrls.map(u => u.finalURL))
+    mappedUrls = mappedUrls.filter(p => p.impression_pixel_json !== '')
+    mappedUrls = mappedUrls.filter(p => p.impression_pixel_json !== 'NULL')
   })
 
-  const noNull = arr.filter(a => a !== 'NULL')
-  const finalArr = noNull.filter(a => a !== '')
-  console.log(finalArr)
-  
   let resArr = [];
   let successCount = 0;
   let count = 0;
-
-  finalArr.map(url => {
-    axios.get(url).then(res => {
+// console.log("mappedUrls:")
+// console.log(mappedUrls)
+mappedUrls.map(url => {
+  const axiosURL = url.impression_pixel_json;
+    axios.get(axiosURL).then(res => {
       console.log("url status:")
+      console.log(axiosURL)
       console.log(res.status)
-      if(res.status == 200){
-        console.log("200")
+      if(res.status >= 200 && res.status <= 399){
       successCount++
       resArr.push({status:res.status,count:successCount})
-      fs.appendFile("data_v1.txt",JSON.stringify(resArr),(err)=>{
+      fs.writeFile("data_" + dataArg + ".txt",JSON.stringify(resArr),(err)=>{
         if(err)console.log(err)
           })
         }
@@ -55,21 +67,23 @@ fs.readFile(__dirname + "/" + process.argv.slice(2), async (err, data) => {
             errURL = err.config.url;
         }
         
+        const lastError = mappedUrls.filter(p =>{ 
+          return p.impression_pixel_json == errURL;
+        })
+        console.log("errURL:")
+        console.log(errURL)
         console.log("error is:")
-        // console.log(err.response.request.res.socket._httpMessage.connection._httpMessage)
-        // console.log(Object.keys(err.request))
-          const lastError = errArr.filter(p =>{
-            return p.impression_pixel_json == errURL;
-          })
+        console.log(lastError)
           
           count++
-          const impPixel = lastError[0]["impression_pixel_json"];
-          const tac_id =lastError[0]["tac"]
-          const lastErrorObj = {impPixel,tac_id,count}
+          const impression_pixel_json = lastError[0]["impression_pixel_json"];
+          const tactic_id =lastError[0]["tactic_id"]
+          const lastErrorObj = {count,tactic_id,impression_pixel_json}
           const myArr = [];
           myArr.push(lastErrorObj)
+          console.log("myArr:")
           console.log(myArr)
-          fs.appendFile("err_v1.txt",JSON.stringify(myArr),(err)=>{
+          fs.appendFile("error_" + errArg + ".txt",JSON.stringify(myArr),(err)=>{
             if(err)console.log(err)
             })
       })
